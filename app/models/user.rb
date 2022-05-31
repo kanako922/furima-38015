7c78c5119a95a78508a345f85c707d1a3f021458
kanako922 <knkokt.922@gmail.com>
2022-05-31 18:41:47 +0900

    ユーザーモデル単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name,
-            :last_name_kana, :first_name_kana, :birthday,
-            presence: true
+  validates :nickname, :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, :password_confirmation, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
